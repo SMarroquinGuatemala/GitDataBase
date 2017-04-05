ef84074b7189a779b95f2fc0c8d038b1292e7e66
SMarroquinGuatemala <smarroquin_kruel@hotmail.com>
2017-04-05 08:43:00 -0600

    Se agrega la creacion de usuario de SQL y el lenguaje predeterminado

--- GitHubDataBase/DDL/droprolemember.sql
+++ GitHubDataBase/DDL/droprolemember.sql
@@ -1,4 +1,14 @@
-USE [DbActivosFijos]
+USE [DbActivosFijosFotos]
 GO
 EXEC sp_droprolemember N'db_ddladmin', N'TiProgramador'
+GO
+
+
+USE [DbActivosFijosFotos]
+GO
+ALTER AUTHORIZATION ON SCHEMA::[db_datareader] TO [TiProgramador]
+GO
+USE [DbActivosFijosFotos]
+GO
+ALTER AUTHORIZATION ON SCHEMA::[db_datawriter] TO [TiProgramador]
 GO
